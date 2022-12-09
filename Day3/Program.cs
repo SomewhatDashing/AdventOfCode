aeffa10637aedec2d032c9f3a6f1ad8c02f03835
Sjöö Patrick (Consultant) <patrick.sjoo@consultant.volvo.com>
2022-12-09 12:47:37 +0100

    Added day 3 and 4

---
+++ Day3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllText(@"..\..\input.txt").Split(new string[] { "\r\n" }, StringSplitOptions.None);
+
+            string values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            int prioSum = 0;
+
+            foreach (string line in lines)
+            {
+                string firstHalf = line.Substring(0, line.Length / 2);
+                string secondHalf = line.Substring(line.Length / 2);
+
+                foreach (char letterFirst in firstHalf)
+                {
+                    if (secondHalf.Contains(letterFirst))
+                    {
+                        prioSum += values.IndexOf(letterFirst) + 1;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(prioSum);
+
+            prioSum = 0;
+
+            // Part 2 - Groups of 3
+            for (int i = 0; i < lines.Length; i += 3)
+            {
+                foreach (char letter in lines[i])
+                {
+                    if (lines[i + 1].Contains(letter) && lines[i + 2].Contains(letter))
+                    {
+                        prioSum += values.IndexOf(letter) + 1;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(prioSum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllText(@"..\..\input.txt").Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            string[] group;
+            int min1, min2, max1, max2;
+            int overlaps1 = 0, overlaps2 = 0;
+
+            foreach (string line in lines)
+            {
+                group = line.Split(',');
+
+                min1 = int.Parse(group[0].Split('-')[0]);
+                min2 = int.Parse(group[1].Split('-')[0]);
+                max1 = int.Parse(group[0].Split('-')[1]);
+                max2 = int.Parse(group[1].Split('-')[1]);
+
+                if ((min1 >= min2 && max1 <= max2) || (min2 >= min1 && max2 <= max1))
+                    overlaps1++;
+
+                if ((min1 >= min2 && min1 <= max2) || (min2 >= min1 && min2 <= max1) || (max1 >= min2 && max1 <= max2) || (max2 >= min1 && max2 <= max1))
+                    overlaps2++;
+            }
+
+            Console.WriteLine(overlaps1);
+            Console.WriteLine(overlaps2);
+            Console.ReadKey();
+        }
+    }
+}
